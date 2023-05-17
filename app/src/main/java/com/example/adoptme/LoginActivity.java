54e5cbdc123dca75410834a96398492926c3c01a
pantgram <pantgram@csd.auth.gr>
2023-05-17 15:38:23 +0300

    make login with firebase

--- app/src/main/java/com/example/adoptme/LoginActivity.java
+++ app/src/main/java/com/example/adoptme/LoginActivity.java
@@ -1,34 +1,62 @@
 package com.example.adoptme;
 
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     Button b1;
-    EditText ed1,ed2;
+    EditText email,password;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         b1 = findViewById(R.id.button);
-        ed1 = findViewById(R.id.editText);
-        ed2 = findViewById(R.id.editText2);
-        b1.setOnClickListener(v -> {
-            if(ed1.getText().toString().equals("admin") &&
-                    ed2.getText().toString().equals("admin")) {
-                Toast.makeText(getApplicationContext(),
-                        "Redirecting...",Toast.LENGTH_SHORT).show();
-            }else{
-                Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
-            }
-        });
+        email = findViewById(R.id.editText);
+        password = findViewById(R.id.editText2);
+        mAuth.signInWithEmailAndPassword(email.toString(), password.toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            goToMainPage();
+                        } else {
+                            /* TO DO */
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    public void goToMainPage() {
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
