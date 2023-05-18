d9331cdb930014ac60b68f2f9b6723b633961276
pantgram <pantgram@csd.auth.gr>
2023-05-18 14:09:57 +0300

    add name in database

---
+++ app/src/main/java/com/example/adoptme/DatabaseHelper.java
@@ -0,0 +1,58 @@
+package com.example.adoptme;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import androidx.annotation.Nullable;
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String databaseName = "AdoptMe.sqlite";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, databaseName, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase MyDatabase) {
+        MyDatabase.execSQL("create Table users(email TEXT primary key, password TEXT, name Text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase MyDB, int i, int i1) {
+        MyDB.execSQL("drop Table if exists users");
+    }
+
+    public Boolean insertData(String email, String password,String name) {
+        SQLiteDatabase MyDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("email", email);
+        contentValues.put("password", password);
+        contentValues.put("name",name);
+        long result = MyDatabase.insert("users", null, contentValues);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Boolean checkEmail(String email) {
+        SQLiteDatabase MyDatabase = this.getWritableDatabase();
+        Cursor cursor = MyDatabase.rawQuery("Select * from users where email = ?", new String[]{email});
+        if (cursor.getCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Boolean checkEmailPassword(String email, String password) {
+        SQLiteDatabase MyDatabase = this.getWritableDatabase();
+        Cursor cursor = MyDatabase.rawQuery("Select * from users where email = ? and password = ?", new String[]{email, password});
+        if (cursor.getCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- app/src/main/java/com/example/adoptme/SignUpActivity.java
+++ app/src/main/java/com/example/adoptme/SignUpActivity.java
@@ -7,10 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class SignUpActivity extends AppCompatActivity {
@@ -19,6 +21,7 @@ public class SignUpActivity extends AppCompatActivity {
     Button submitBtn;
 
     TextView goToLogIn;
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,32 +34,38 @@ public class SignUpActivity extends AppCompatActivity {
         submitBtn = findViewById(R.id.signUpBtn);
         goToLogIn = findViewById(R.id.signUpToLogIn);
 
-
+        databaseHelper = new DatabaseHelper(this);
+        submitBtn.setOnClickListener(view -> {
+            String emailStr, passwordStr, passwordVerStr,nameStr;
+            nameStr=name.getText().toString();
+            emailStr = email.getText().toString();
+            passwordStr = password.getText().toString();
+            passwordVerStr = passwordVerification.getText().toString();
+            if (emailStr.equals("") || passwordStr.equals("") || passwordVerStr.equals(""))
+                Toast.makeText(SignUpActivity.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
+            else {
+                if (passwordStr.equals(passwordVerStr)) {
+                    Boolean checkUserEmail = databaseHelper.checkEmail(emailStr);
+                    if (!checkUserEmail) {
+                        Boolean insert = databaseHelper.insertData(emailStr, passwordStr,nameStr);
+                        if (insert) {
+                            Toast.makeText(SignUpActivity.this, "Signup Successfully!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Signup Failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(SignUpActivity.this, "User already exists! Please login", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(SignUpActivity.this, "Invalid Password!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    public void signUp(View view) {
-        String email, name, password, passwordVerification;
-        name = String.valueOf(this.name.getText());
-        email = String.valueOf(this.email.getText());
-        password = String.valueOf(this.password.getText());
-        passwordVerification = String.valueOf(this.passwordVerification.getText());
 
-        // TODO: Να το ολοκληρώσω
-        if (!checkForEmptyStrings(name, email, password, passwordVerification)) {
-
-        }
-
-        // TODO: Να το ολοκληρώσω
-        if (!password.equals(passwordVerification)) {
-
-        }
-
-    }
-
-    private boolean checkForEmptyStrings(String name, String email, String password, String password2) {
-        return !TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) &&
-                !TextUtils.isEmpty(password) && !TextUtils.isEmpty(password2);
-    }
 
     public void goToLogInPage(View view) {
         Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
