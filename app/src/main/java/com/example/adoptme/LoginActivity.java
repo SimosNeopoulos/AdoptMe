59ca7a1982ba20a29a264ad8963ef1158b1212d7
SimosNeopoulos <simneo22@gmail.com>
2023-05-20 16:59:18 +0300

    Implemented logInUser function

--- app/src/main/java/com/example/adoptme/LoginActivity.java
+++ app/src/main/java/com/example/adoptme/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.adoptme;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,17 +16,48 @@ public class LoginActivity extends AppCompatActivity {
     Button b1;
     EditText email;
     EditText password;
+    private DatabaseHelper databaseHelper;
+    private SessionManager sessionManager;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        sessionManager = new SessionManager(LoginActivity.this);
         email = findViewById(R.id.LoginEmail);
         password = findViewById(R.id.PasswordEmail);
         b1 = findViewById(R.id.button);
+        databaseHelper = new DatabaseHelper(this);
+        b1.setOnClickListener(this::logInUser);
 
 
     }
 
+    private void goToMainPage() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void logInUser(View view) {
+        String emailTxt, passwordTxt;
+        emailTxt = email.getText().toString();
+        passwordTxt = password.getText().toString();
+        if (emailTxt.equals("") || passwordTxt.equals("")) {
+            Toast.makeText(LoginActivity.this, "All fields are mandatory",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        User user = databaseHelper.checkEmailPassword(emailTxt, passwordTxt);
+        if (user == null) {
+            Toast.makeText(LoginActivity.this, "User was not found",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        sessionManager.saveSession(user);
+        goToMainPage();
+
+    }
 }
