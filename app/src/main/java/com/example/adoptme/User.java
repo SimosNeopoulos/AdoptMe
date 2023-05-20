b1cc574f6ddb16b8487187423990595a63091a34
SimosNeopoulos <simneo22@gmail.com>
2023-05-20 17:00:51 +0300

    Added SessionManager and User classes

---
+++ app/src/main/java/com/example/adoptme/User.java
@@ -0,0 +1,47 @@
+package com.example.adoptme;
+
+public class User {
+    private int id;
+    private String name, email, password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(int id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
