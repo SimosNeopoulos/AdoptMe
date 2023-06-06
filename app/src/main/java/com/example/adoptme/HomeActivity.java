b8ad1ce221a1c9d8bb4b16d03d043838d83fb71e
pantgram <pantgram@csd.auth.gr>
2023-06-06 17:12:51 +0300

    little bugs on view post and correct layouts

--- app/src/main/java/com/example/adoptme/HomeActivity.java
+++ app/src/main/java/com/example/adoptme/HomeActivity.java
@@ -9,6 +9,6 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_post);
+        setContentView(R.layout.activity_home);
     }
 }
--- app/src/main/java/com/example/adoptme/ViewPostActivity.java
+++ app/src/main/java/com/example/adoptme/ViewPostActivity.java
@@ -9,6 +9,6 @@ public class ViewPostActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_view_posts);
     }
 }
