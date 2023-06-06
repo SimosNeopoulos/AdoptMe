1c07b706bc9aeb17464d50cb611e75ac35313bf6
Nikos Natsopoulos <natsniko@csd.auth.gr>
2023-06-06 16:23:22 +0300

    View Post Activity Add Post Activity

--- app/src/main/java/com/example/adoptme/ViewPostActivity.java
+++ app/src/main/java/com/example/adoptme/ViewPostActivity.java
@@ -1,16 +1,14 @@
-//THE MAIN PAGE OF THE APP
 package com.example.adoptme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ViewPostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
