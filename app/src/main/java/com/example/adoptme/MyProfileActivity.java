5601904dab8ab096cbb1890d0349db9280ce9877
pantgram <pantgram@csd.auth.gr>
2023-06-12 13:28:49 +0300

    Add My profile

--- app/src/main/java/com/example/adoptme/MyProfileActivity.java
+++ app/src/main/java/com/example/adoptme/MyProfileActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ViewPostActivity extends AppCompatActivity {
+public class MyProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_post);
+        setContentView(R.layout.activity_my_profile);
     }
 }
