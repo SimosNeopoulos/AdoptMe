8c4464372df188a877817a23944d4279c490cb88
pantgram <pantgram@csd.auth.gr>
2023-06-13 13:06:03 +0300

    menu in AddPost, editPost fixed myProfile button bug

--- app/src/main/java/com/example/adoptme/EditPostActivity.java
+++ app/src/main/java/com/example/adoptme/EditPostActivity.java
@@ -1,12 +1,18 @@
 package com.example.adoptme;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
@@ -15,6 +21,9 @@ public class EditPostActivity extends AppCompatActivity {
     private int postId;
     private EditText name, age, species, description, telephone, town;
     private Button editPostBtn;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
     //private SessionManager sessionManager;
     DatabaseHelper databaseHelper;
 
@@ -40,8 +49,34 @@ public class EditPostActivity extends AppCompatActivity {
         if (intent != null) {
             setTextFields(intent);
         }
+        setUpToolbar();
+        navigationView = findViewById(R.id.navigation_menu);
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            if(menuItem.getItemId() == R.id.mainpage){
+                Intent intentMain = new Intent(EditPostActivity.this, MainActivity.class);
+                startActivity(intentMain);
+
+            }else if(menuItem.getItemId() == R.id.my_profile){
+                Intent intentDonation = new Intent(EditPostActivity.this, MyProfileActivity.class);
+                startActivity(intentDonation);
+            }else if(menuItem.getItemId() == R.id.logout){
+                Toast.makeText(this, "Επιτυχής Αποσύνδεση", Toast.LENGTH_SHORT).show();
+                Intent intentDonation = new Intent(EditPostActivity.this, LoginActivity.class);
+                startActivity(intentDonation);
+            }
+            return false;
+        });
+
     }
 
+    public void setUpToolbar() {
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        Toolbar toolbar = findViewById(R.id.editPostToolbar);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState( );
+    }
     private void setTextFields(Intent intent) {
         ArrayList<String> data = intent.getStringArrayListExtra("postData");
         if (data == null)
